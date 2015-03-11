e80174363a1a670946b0d337bf5285938fdcc30a
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-11 18:36:05 -0400

    Alllmost works except doesn't update factors for subsequent user inputs

--- project-04/euler-04.js
+++ project-04/euler-04.js
@@ -90,8 +90,47 @@ var getLargestPalPdct = function(digits) {
 
 //alert(minPdct(3));
 
-alert("The largest palindrome product is " + getLargestPalPdct(3) + " and the factors are " + factors[0] + " and " + factors[1])
+//alert("The largest palindrome product is " + getLargestPalPdct(3) + " and the factors are " + factors[0] + " and " + factors[1])
 
 //PROBLEM: Infinite loop if digits = 4
 //alert(getLargestPalPdct(4));
 
+/*This ties the button we made to a JS var button*/
+var button = document.getElementById("button");
+
+//This says, when the button is clicked do the function handleSubmit
+//addEventListener expects a function that takes one argument, 
+//and that argument is the event
+button.addEventListener("click", handleSubmit);
+
+//This is the function with just one arg which is event
+function handleSubmit (e) {
+  var userInput = document.getElementById("input");
+  //This takes the stuff from form (userInput.value) and turns it into an Int
+  var inputNum = parseInt(userInput.value);
+
+  if (isNaN(inputNum)) {
+    alert("Error: Please input a number");
+    return;
+  } 
+  // if there's no period, indexOf returns -1, so if it is not equal
+  // to -1, then there is a period
+  else if (userInput.value.indexOf(".") !== -1 ) {
+    alert("Error: Please input a positive integer... \nI don't know how to handle decimals yet");
+    return;
+  }
+  else if (userInput.value.indexOf("-") !== -1) {
+    alert("Error: Please input a positive integer... \nI don't know how to handle negative numbers yet");
+    return;
+  }
+
+  var largestPalPdct = getLargestPalPdct(inputNum);
+
+  var answer1 = document.getElementById("answer1");
+ 
+  answer1.innerHTML = largestPalPdct;
+ 
+  var answer2 = document.getElementById("answer2");
+ 
+  answer2.innerHTML = factors[0] + " and " + factors[1];
+}
