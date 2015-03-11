6ab013c7c2f3e5b445233e3eada22946570d27d6
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-11 19:44:33 -0400

    Nothing works yet

---
+++ project-05/euler-05.js
@@ -0,0 +1,15 @@
+/* Pseudocode:
+
+Will definitely want to put things in terms of maxDivisor
+Same as least common multiple?
+
+multiple = 0
+i = 0
+
+while i < maxDivisor
+if (multiple % i !== 0) {
+	multiple++;
+}
+else if (multiple % i === 0)
+
+*/
---
+++ project-05/euler-05.rb
@@ -0,0 +1,21 @@
+=begin
+1. We're gonna get an array of divisors
+
+2. We're gonna get a multiple for the first divisor
+
+3. We're gonna get the next divisor and check if it divides the multiple evenly
+
+4. If not, increment multiple by... divisors[1]
+=end
+
+
+class Multiple
+	def get_divisors(max)
+		divisors = (1..max).to_a
+	end
+
+	def get_multiple(divisor)
+		
+	end
+
+end
